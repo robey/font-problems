904ac1266b54db2a618b6cb10daf7d95b18aca95
Robey Pointer <robeypointer@gmail.com>
2014-06-29 15:20:38 -0700

    just use the tom-thumb fmap now.

--- mktom.sh
+++ mktom.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./bin/font-problems "$1" -P tom-thumb.psf -A --map 0-x7f,x2500,x2502,x250c,x2510,x2514,x2518,x251c,x2524,x252c,x2534,x253c,x2580,x2584,x2588,x258c,x2590,x2592
+./bin/font-problems "$1" --map "${1%.bmp}.fmap" -A -P
