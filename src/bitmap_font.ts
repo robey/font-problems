86a35a7c7b963ab1c29593af8de7b08777ec30b1
Robey Pointer <robeypointer@gmail.com>
2018-03-06 08:00:52 -0800

    tweak sniffBoundaries to work for Terminus.

--- src/bitmap_font.ts
+++ src/bitmap_font.ts
@@ -128,7 +128,7 @@ export class BitmapFont {
  */
 export function sniffBoundaries(image: Framebuffer): { width: number, height: number } {
   function average(list: number[]): number {
-    return list.reduce((a, b) => a + b) / list.length;
+    return list.reduce((a, b) => a + b, 0) / list.length;
   }
 
   // convert each row & column into a number representing the "average brightness".
@@ -138,12 +138,12 @@ export function sniffBoundaries(image: Framebuffer): { width: number, height: nu
   // average the "average brightness" values for every line along an
   // interval, with a small penalty to larger intervals.
   function checkInterval(lines: number[], interval: number): number {
-    return average(range(interval, lines.length, interval).map(i => lines[i - 1])) * Math.pow(0.99, interval);
+    return average(range(interval, lines.length, interval).map(i => lines[i - 1])) * Math.pow(0.995, interval);
   }
 
   function pickBestInterval(lines: number[], low: number, high: number): number {
     // try intervals that evenly divide into the number of lines.
-    const intervals = range(low, high).filter(n => lines.length % n == 0);
+    const intervals = range(low, high).filter(n => n < lines.length && lines.length % n == 0);
     return intervals.map(n => ({ n, weight: checkInterval(lines, n) })).sort((a, b) => {
       // by highest weight, and in case of tie, by smallest n.
       return (a.weight == b.weight) ? a.n - b.n : b.weight - a.weight;
@@ -151,7 +151,7 @@ export function sniffBoundaries(image: Framebuffer): { width: number, height: nu
   }
 
   return {
-    width: pickBestInterval(columns, 4, 13),
-    height: pickBestInterval(rows, 6, 17)
+    width: pickBestInterval(columns, 4, 17),
+    height: pickBestInterval(rows, 6, 33)
   };
 }
